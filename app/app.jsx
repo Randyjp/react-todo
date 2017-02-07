872fa441f6414773e75baab0acc8467638dcc7b0
randyjp <randyjp125@gmail.com>
2017-02-07 18:58:15 -0500

    Creating the main component for the app

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-//eqivalent to var Router = require('react-router').Route
-//good for all the variables
 var {Router, Route, IndexRoute, browserHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 //load foundation
 $(document).foundation(); //fire-up foundation
 
@@ -11,6 +10,6 @@ $(document).foundation(); //fire-up foundation
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-	<p>Boler plate 3</p>,
+	<TodoApp/>,
 	document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Fucking component
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
