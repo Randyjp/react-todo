6a121ccf50cf6d9c9d37613a836cad75bf09b74e
randyjp <randyjp125@gmail.com>
2017-03-01 18:30:29 -0500

    move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-     apiKey: "AIzaSyDyG-w7ISYhM0HSP7n5dpQWJcSZGJOIBmU",
-     authDomain: "randy-todo-app.firebaseapp.com",
-     databaseURL: "https://randy-todo-app.firebaseio.com",
-     storageBucket: "randy-todo-app.appspot.com",
-     messagingSenderId: "683327424945"
+     apiKey: process.env.API_KEY,
+     authDomain: process.env.AUTH_DOMAIN,
+     databaseURL: process.env.DATABASE_URL,
+     storageBucket: process.env.STORAGE_BUCKET,
+     messagingSenderId: process.env.MESSAGING_SENDER_ID
    };
 
    firebase.initializeApp(config);
