171ee34699aed09ed6edaa88d9f3db832b01af1f
randyjp <randyjp125@gmail.com>
2017-03-01 19:55:06 -0500

    Adding the login component and styles

---
+++ app/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="page-title">Todo App</h1>
+        <div className="row">
+          <div className="columns small-centered small-10 medium-6 large-4">
+            <div className="callout callout-auth">
+              <h3>Login</h3>
+              <p>Login with GitHub account below</p>
+              <button className="button">Login With GitHub</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
