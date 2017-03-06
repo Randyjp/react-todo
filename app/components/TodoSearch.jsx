ad2be64e3674445df6a8c5ffab58078e460147be
randyjp <randyjp125@gmail.com>
2017-03-05 22:59:26 -0500

    refactoring TodoSearch to use es6 syntax

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -2,32 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from 'actions';
 
-// export var TodoSearch = React.createClass({
-//   render: function() {
-//     var {dispatch, showCompleted, searchText} = this.props;
-//
-//     return (
-//       <div className="container__header">
-//         <div>
-//           <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
-//             var searchText = this.refs.searchText.value;
-//             dispatch(actions.setSearchText(searchText));
-//           }}/>
-//         </div>
-//         <div>
-//           <label>
-//             <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
-//               dispatch(actions.toggleShowCompleted());
-//             }}/>
-//             Show completed todos
-//           </label>
-//         </div>
-//       </div>
-//     );
-//   }
-// });
-
-export class TodoSearch {
+export class TodoSearch extends React.Component {
   render () {
     var {dispatch, showCompleted, searchText} = this.props;
 
